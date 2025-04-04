7f8d20bc9fceb1d60829e36ef46fc7b5f566285d
Taleiven Kattan <taleiven@gmail.com>
2025-04-04 21:33:40 +0200

    Add Angular Material theme and enhance layout with new sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatCard, MatCardContent, MatCardTitle} from '@angular/material/card';
+import {MatAnchor} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatCard, MatCardTitle, MatCardContent, MatAnchor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
